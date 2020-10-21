0c281d3976d9d995a899dcf06275401c4208487a
tferfce <t.ferfce@gmail.com>
2020-10-21 19:07:06 +0300

    "Data Classify"

--- src/main/java/gr/codehub/team5/resource/PatientDataSpecifyResource.java
+++ src/main/java/gr/codehub/team5/resource/PatientDataSpecifyResource.java
@@ -1,9 +1,14 @@
 package gr.codehub.team5.resource;
 
+import gr.codehub.team5.exceptions.BadEntityException;
 import gr.codehub.team5.exceptions.NotFoundException;
+import gr.codehub.team5.representation.PatientDataRepresentation;
 import org.restlet.resource.Delete;
+import org.restlet.resource.Put;
 
 public interface PatientDataSpecifyResource {
     @Delete
     void deleteSpecificData() throws NotFoundException;
+    @Put
+    PatientDataRepresentation updatePData(PatientDataRepresentation patientDataRepresentation) throws NotFoundException, BadEntityException;
 }
--- src/main/java/gr/codehub/team5/resource/impl/PatientDataSpecifyResourceImpl.java
+++ src/main/java/gr/codehub/team5/resource/impl/PatientDataSpecifyResourceImpl.java
@@ -1,10 +1,12 @@
 package gr.codehub.team5.resource.impl;
 
 import gr.codehub.team5.Model.PatientData;
+import gr.codehub.team5.exceptions.BadEntityException;
 import gr.codehub.team5.exceptions.NotFoundException;
 import gr.codehub.team5.jpa.SacchonJpa;
 import gr.codehub.team5.repository.PatientDataRepository;
 import gr.codehub.team5.repository.PatientRepository;
+import gr.codehub.team5.representation.PatientDataRepresentation;
 import gr.codehub.team5.resource.PatientDataSpecifyResource;
 import org.restlet.resource.ResourceException;
 import org.restlet.resource.ServerResource;
@@ -45,5 +47,18 @@ public class PatientDataSpecifyResourceImpl extends ServerResource implements Pa
         query.setParameter("param",this.id);
         List<PatientData> pdataList = query.getResultList();
         patientDataRepository.deleteById(pdataList.get(listId-1).getId());
+    }
+
+    @Override
+    public PatientDataRepresentation updatePData(PatientDataRepresentation patientDataRepresentation) throws NotFoundException, BadEntityException {
+        TypedQuery<PatientData> query = em.createQuery("FROM PatientData P WHERE pData_id=:param", PatientData.class);
+        query.setParameter("param",this.id);
+        List<PatientData> pdataList = query.getResultList();
+        PatientData patientData = pdataList.get(listId-1);
+        patientData.setBloodGlucose(patientDataRepresentation.getBloodGlucose());
+        patientData.setCarbIntake(patientDataRepresentation.getCarbIntake());
+        patientDataRepository.save(patientData);
+
+        return PatientDataRepresentation.getDataRepresentation(patientData);
     }
 }
