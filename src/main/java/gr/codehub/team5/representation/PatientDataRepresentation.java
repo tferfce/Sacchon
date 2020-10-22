6188d64f7bce5c984b06031af529b4bf1564eb86
tferfce <t.ferfce@gmail.com>
2020-10-22 21:38:49 +0300

    "Data Classify"

--- src/main/java/gr/codehub/team5/representation/PatientDataRepresentation.java
+++ src/main/java/gr/codehub/team5/representation/PatientDataRepresentation.java
@@ -6,12 +6,14 @@ import lombok.Data;
 import java.util.Date;
 @Data
 public class PatientDataRepresentation {
+    private long id;
     private double carbIntake;
     private double bloodGlucose;
     private Date date = new Date();
 
     public static PatientDataRepresentation getDataRepresentation(PatientData patientData){
         PatientDataRepresentation patientDataRepresentation = new PatientDataRepresentation();
+        patientDataRepresentation.setId(patientData.getId());
         patientDataRepresentation.setCarbIntake(patientData.getCarbIntake());
         patientDataRepresentation.setBloodGlucose(patientData.getBloodGlucose());
         return patientDataRepresentation;
--- src/main/java/gr/codehub/team5/representation/PatientRepresentation.java
+++ src/main/java/gr/codehub/team5/representation/PatientRepresentation.java
@@ -5,6 +5,7 @@ import lombok.Data;
 
 @Data
 public class PatientRepresentation {
+    private long id;
     private String firstName;
     private String lastName;
     private String gender;
@@ -15,7 +16,6 @@ public class PatientRepresentation {
 
     public static Patient getPatient(PatientRepresentation patientRepresentation){
         Patient patient = new Patient();
-
         patient.setFirstName(patientRepresentation.getFirstName());
         patient.setLastName(patientRepresentation.getLastName());
         patient.setGender(patientRepresentation.getGender());
@@ -27,7 +27,7 @@ public class PatientRepresentation {
 
     public static PatientRepresentation getPatientRepresentation(Patient patient){
         PatientRepresentation patientRepresentation = new PatientRepresentation();
-
+        patientRepresentation.setId(patient.getId());
         patientRepresentation.setFirstName(patient.getFirstName());
         patientRepresentation.setLastName(patient.getLastName());
         patientRepresentation.setGender(patient.getGender());
