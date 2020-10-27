d11072fa6858a54ea113648a91aa8d3dc5575654
Haritini Zisi <charitinizisi@gmail.com>
2020-10-27 13:16:47 +0200

    Doctor with not activity + backend

--- src/main/java/gr/codehub/team5/resource/impl/DoctorNoActivityResourceImpl.java
+++ src/main/java/gr/codehub/team5/resource/impl/DoctorNoActivityResourceImpl.java
@@ -1,7 +1,5 @@
 package gr.codehub.team5.resource.impl;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import gr.codehub.team5.Model.Consultations;
 import gr.codehub.team5.Model.Doctor;
 import gr.codehub.team5.exceptions.NotFoundException;
@@ -17,7 +15,10 @@ import javax.persistence.EntityManager;
 import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
 
 public class DoctorNoActivityResourceImpl extends ServerResource implements DoctorNoActivityResource {
 
@@ -43,7 +44,9 @@ public class DoctorNoActivityResourceImpl extends ServerResource implements Doct
     }
 
     @Override
-    public List<DoctorRepresentation> getDoctorsWithNoActivity(String dates) throws NotFoundException, ParseException, IOException {
+    public List<DoctorRepresentation> getDoctorsWithNoActivity() throws NotFoundException, ParseException, IOException {
+        String paramValue1 = getQueryValue("fromDate");
+        String paramValue2 = getQueryValue("toDate");
         // Get all doctors ids
         List<Doctor> doctors = doctorRepository.findAll();
         List<Long> idsFromDoctor = new ArrayList<>();
@@ -51,12 +54,12 @@ public class DoctorNoActivityResourceImpl extends ServerResource implements Doct
 
         // Get doctors ids in date range from Consultation
         // A) String to Json object
-        ObjectMapper mapper = new ObjectMapper();
-        Map<String,Object> map = mapper.readValue(dates, Map.class);
-        JsonNode rootNode = mapper.readTree(dates);
+//        ObjectMapper mapper = new ObjectMapper();
+//        Map<String,Object> map = mapper.readValue(dates, Map.class);
+//        JsonNode rootNode = mapper.readTree(dates);
         // B) Json values to Dates
-        Date dateFrom = new SimpleDateFormat("yyyy/MM/dd").parse(rootNode.get("fromDate").asText());
-        Date dateTo = new SimpleDateFormat("yyyy/MM/dd").parse(rootNode.get("toDate").asText());
+        Date dateFrom = new SimpleDateFormat("yyyy/MM/dd").parse(paramValue1);
+        Date dateTo = new SimpleDateFormat("yyyy/MM/dd").parse(paramValue2);
         // C) Add 1 day to toDate
         Calendar c = Calendar.getInstance();
         c.setTime(dateTo);
