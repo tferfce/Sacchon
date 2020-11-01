97a398c698ca4f764b54fb72cd7f9c582026d02f
tferfce <t.ferfce@gmail.com>
2020-11-01 18:07:24 +0200

    Inserting Auth

--- src/main/java/gr/codehub/team5/router/CustomRouter.java
+++ src/main/java/gr/codehub/team5/router/CustomRouter.java
@@ -21,7 +21,7 @@ public class CustomRouter {
     public Router createApiRouter() {
         Router router = new Router(application.getContext());
         //Patient
-        router.attach("/patient/{id}/data", PatientDataResourceImpl.class);//Admin Privilege also
+        router.attach("/patient/{id}/data", PatientDataResourceImpl.class);
         router.attach("/patient/{id}", PatientResourceImpl.class);
         router.attach("/patient/{id}/data/edit", PatientDataSpecifyResourceImpl.class);
         router.attach("/patient/{id}/avg", PatientAvgDataImpl.class);
