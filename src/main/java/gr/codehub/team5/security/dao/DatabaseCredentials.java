e5ef2f938e8d11a74312af5a8cb32c6f1c06b61b
tferfce <t.ferfce@gmail.com>
2020-11-02 18:59:34 +0200

    "name changes"

--- src/main/java/gr/codehub/team5/security/dao/DatabaseCredentials.java
+++ src/main/java/gr/codehub/team5/security/dao/DatabaseCredentials.java
@@ -8,5 +8,5 @@ public class DatabaseCredentials {
     public static final String URL = "jdbc:sqlserver://localhost;databaseName=sacchon;";
     public static final String USER = "sa";
     public static final String DRIVER = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
-    public static final String PASSWORD = "passw0rd";
+    public static final String PASSWORD = "admin!@#123";
 }
